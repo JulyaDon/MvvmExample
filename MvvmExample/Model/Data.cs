7d9fde65defb2176183c34eb52f141626cba3268
JulyaDon <julya.p.don@gmail.com>
2019-03-23 18:40:22 +0200

    Initial commit

---
+++ MvvmExample/Model/Data.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmExample.Model
+{
+    public class Data : BindableBase
+    {
+        private readonly ObservableCollection<int> _myValues = new ObservableCollection<int>();
+
+        public readonly ReadOnlyObservableCollection<int> MyPublicValues;
+
+        public Data()
+        {
+            MyPublicValues = new ReadOnlyObservableCollection<int>(_myValues);
+        }
+
+        public void AddValue(int value)
+        {
+            _myValues.Add(value);
+            RaisePropertyChanged("Sum");
+        }
+
+        public void RemoveValue(int index)
+        {
+            if (index >= 0 && index < _myValues.Count)
+            {
+                _myValues.RemoveAt(index);
+                RaisePropertyChanged("Sum");
+            }
+        }
+
+        public int Sum => MyPublicValues.Sum();
+    }
+}
---
+++ MvvmExample/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using MvvmExample.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MvvmExample.ViewModel
+{
+    public class MainViewModel : BindableBase
+    {
+        readonly Data _data = new Data();
+
+        public MainViewModel()
+        {
+            _data.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
+
+            AddCommand = new DelegateCommand<string> (str => {
+                int ival;
+                if (int.TryParse(str, out ival)) _data.AddValue(ival);
+            });
+            RemoveCommand = new DelegateCommand<int?>(i =>
+           {
+               if (i.HasValue) _data.RemoveValue(i.Value);
+           });
+        }
+
+        public DelegateCommand<string> AddCommand { get; }
+        public DelegateCommand<int?> RemoveCommand { get; }
+        public int Sum => _data.Sum;
+        public ReadOnlyObservableCollection<int> MyValues => _data.MyPublicValues;
+    }
+}
